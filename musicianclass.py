7f326e4881835240afd7d18f950210cc6ad929b8
yeungpirate <yeungpirate@gmail.com>
2018-02-13 09:17:04 +0000

    Band update

--- musicianclass.py
+++ musicianclass.py
@@ -1,10 +1,12 @@
+members = ["Tom", "Mark", "Travis"]
+
 class Musician(object):
-    def __init__(self, sound):
+    def __init__(self, sounds):
         self.sounds = sounds
         
     def solo(self, length):
         for i in range(length):
-            print(self.sounds[i % len(self.sounds)], end =" ")
+            print(self.sounds[i % len(self.sounds)], end = " ")
         print()
         
 class Bassist(Musician):
@@ -23,11 +25,28 @@ class Drummer(Musician):
     def __init__(self):
         super().__init__(["Bang", "Bam", "Boom"])
         
-    def count():
+    def count(self):
         n = 1
-            while n <= 4
-                print(n)
+        while n <= 4:
+            print(n)
             n += 1
             
-    def spontaneously_combust():
+    def spontaneously_combust(self):
+        print('"Spontaneously Combusts"')
         
+class Band(object):
+    def hire(self):
+        for people in members:
+            members.append(input())
+            return members
+        
+    def fire():
+        for people in members:
+            members.remove(input())
+            return members
+        
+    def play_music(self):
+        Drummer.count(self)
+        # Drummer.solo(3)
+        # Guitarist.solo(3)
+        # Bassist.solo(3)
